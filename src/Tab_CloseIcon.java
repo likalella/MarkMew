7aa9fb67baf82d1dffa4e2a673cc8cefffd977b5
HyeJeong <hj41790@naver.com>
2017-07-30 21:27:13 +0900

    Revert "아이콘 주석 삭제"
    This reverts commit 5f1ef82c90645a9b7e0420dc8c6c8985fc07fe60.

--- src/Tab_CloseIcon.java
+++ src/Tab_CloseIcon.java
@@ -6,8 +6,12 @@ public class Tab_CloseIcon implements Icon {
     @Override
     public void paintIcon(Component c, Graphics g, int x, int y) {
         g.setColor(Color.red);
+//        g.drawRect(0,0,17,17);
         g.drawLine(5,5, getIconWidth()-6, getIconHeight()-6);
         g.drawLine(getIconWidth()-6, 5, 5,getIconHeight()-6);
+
+//        g.drawLine(0,0, getIconWidth()-5, getIconHeight()-5);
+//        g.drawLine(getIconWidth()-5, 0,0,getIconHeight()-5);
     }
 
     @Override
